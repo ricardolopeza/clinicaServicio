df63f3054265f8bf79b4252cbb64aa04aa0452a6
Rlopez <ricardofla1998@gmail.com>
2026-04-08 12:00:52 -0600

    primer commit

---
+++ src/main/java/com/mx/Clinicas/entidad/Clinica.java
@@ -0,0 +1,20 @@
+package com.mx.Clinicas.entidad;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Clinicas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Clinica {
+	@Id
+	private long numero;
+	private String nombre;
+	private String ciudad;
+}
---
+++ src/main/java/com/mx/Clinicas/service/ClinicaService.java
@@ -0,0 +1,5 @@
+package com.mx.Clinicas.service;
+
+public class ClinicaService {
+
+}
